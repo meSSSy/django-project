d07acd6c20d32a2a57c77310fbbd18e94e2976ca
Chris Parry <chrisparry07@live.co.uk>
2024-09-10 14:29:31 +0000

    Deployed to Heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w$=$y+eiku-mf%-w$=5utd-cdi0^b-8fd#i^*fwx+#jk0)t=1('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-messsy-djangoproject-qdas9xpe2s2.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-messsy-djangoproject-qdas9xpe2s2.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
